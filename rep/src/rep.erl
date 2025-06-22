c29685c5e82427f226c42a8fee095f8a7567226e
kk2gvb <n1g.d@ya.ru>
2025-06-23 01:29:23 +0500

    well done

--- rep/src/rep.erl
+++ rep/src/rep.erl
@@ -5,7 +5,9 @@ start() ->
     application:start(chumak),
     {ok, Socket} = chumak:socket(rep),
     {ok, _Pid} = chumak:bind(Socket, tcp, "0.0.0.0", 5015),
+    %% 10 сообщений, сделано только для демонстрации
     loop(Socket, 10),
+    %% Остановка сокета, сделано также только для демонстрации
     gen_server:stop(Socket).
 
 loop(_, 0) ->
@@ -18,7 +20,7 @@ loop(Socket, Count) when Count > 0 ->
     Reversed = lists:reverse(binary_to_list(Msg)),
     ReversedBin = list_to_binary(Reversed),
 
-    %%timer:sleep(1000),
+    timer:sleep(1000),
     chumak:send(Socket, ReversedBin),
 
     loop(Socket, Count - 1).
